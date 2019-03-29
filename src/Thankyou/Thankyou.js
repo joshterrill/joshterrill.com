35f592cdc488fb1d4b3823bdf5b171989a149d72
Josh Terrill <joshterrill.dev@gmail.com>
2019-03-29 15:32:33 -0700

    removed old site

--- src/Thankyou/Thankyou.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export class Thankyou extends Component {
-  render() {
-    return (
-      <h2>Thank you, we have received your submission.</h2>
-    )
-  }
-}
